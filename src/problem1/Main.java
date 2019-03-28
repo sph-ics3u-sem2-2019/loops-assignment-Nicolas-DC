ac1c9509eb2ee67ad0db2f336ef8af634cafa622
Nicolas-DC <nicolas.dela-cruz@ocsbstudent.ca>
2019-03-28 09:56:26 -0400

    Fniish prob 1. Trying if at problem 2.

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -5,6 +5,21 @@ public class Main {
 	public static void main(String[] args) {
 		// Read a value. Print out from 1 to the input
 		Scanner in=new Scanner(System.in);
+		
+		
+		int input = 0 ;				
+		System.out.println( "Give something. Please. I'm poor" ) ;
+		int x = in.nextInt() ;
+		
+		
+		
+		while (x>input) { 
+			input=input+1  ;
+			System.out.println(input);
+		}
+			
 	}
-
+	
+	
+	
 }
--- src/problem2/Main.java
+++ src/problem2/Main.java
@@ -6,6 +6,28 @@ public class Main {
 		// Read in two numbers. Add up all the values between them
 		Scanner in=new Scanner(System.in);
 
+		System.out.println(" Give first nubmer");
+		int y = in.nextInt() ; 
+		
+		System.out.println( "Give a second number") ;
+		int x = in.nextInt() ; 
+		
+		int w = 0 ;
+	if ( y>x ) {
+		
+		for ( int t=y; t >= x; t-- ) {
+			w = y - t;
+		 System.out.println( w );
+		}
+	}
+	 else {
+		
+		for ( int z = y; z <= x; z++ ) {
+			w= w + z ; 
+			
+			 } 
+		System.out.println( w ) ; 
+	}
 	}
 
 }
