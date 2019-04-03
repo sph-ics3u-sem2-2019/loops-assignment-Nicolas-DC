0437f2fb412a0cc9f779b1edc1b5ddb7a0eb7899
Nicolas-DC <nicolas.dela-cruz@ocsbstudent.ca>
2019-04-03 09:54:23 -0400

    Trying to figure out problem 5

--- src/problem5/Main.java
+++ src/problem5/Main.java
@@ -5,6 +5,20 @@ public class Main {
 	public static void main(String[] args) {
 		// Read the string and output in reverse case.
 		Scanner in=new Scanner(System.in);
+		
+		
+		System.out.println( "gIVE mE a wORD");
+		String word = in.nextLine() ; 
+		
+		
+		
+		for (int i = 0; i < word.length(); i++) {
+			
+			
+		}   
+/*		char x = 'g' ; 
+		x=(char) (x+5) ;
+		System.out.println(x);  */
 	}
 
 }
