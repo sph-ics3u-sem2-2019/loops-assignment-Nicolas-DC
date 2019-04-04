618aad97dc1e5a67db7d116b394492283f5b8841
Nicolas-DC <nicolas.dela-cruz@ocsbstudent.ca>
2019-04-04 11:18:25 -0400

    finished 5

--- src/problem5/Main.java
+++ src/problem5/Main.java
@@ -13,6 +13,20 @@ public class Main {
 		
 		
 		for (int i = 0; i < word.length(); i++) {
+			char letter=word.charAt(i);
+			boolean upper=Character.isUpperCase(letter); 
+			boolean lower=Character.isLowerCase(letter);
+			
+			
+			if (upper==true) {
+				letter=Character.toLowerCase(letter) ;
+				System.out.print (letter);
+			}   
+			else if (lower==true) {
+				letter=Character.toUpperCase(letter) ;
+				System.out.print (letter);	
+			}
+			else { System.out.print(letter); }
 			
 			
 		}   
